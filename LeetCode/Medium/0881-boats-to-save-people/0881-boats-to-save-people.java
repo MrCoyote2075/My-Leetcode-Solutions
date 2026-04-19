b617e9f1b3a51359fae97bb63cba55da49403d91
Dhanush N <mrcoyote2075@gmail.com>
2026-04-19 20:10:38 +0530

    0881-boats-to-save-people Medium .java 20 ms (84.04%) Array 04-19-2026

---
+++ LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] arr, int x) {
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        int l = 0, r = n - 1;
+
+        int ans = 0;
+        while (l < r) {
+            if (arr[l] + arr[r] <= x) {
+                l++;
+                r--;
+            } else
+                r--;
+
+            ans++;
+        }
+
+        if (l == r)
+            ans++;
+
+        return ans;
+    }
+}
