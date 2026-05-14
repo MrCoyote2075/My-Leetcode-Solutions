86aa1c5ee580891e7aa25a127a54141aaed0a1e2
Dhanush N <mrcoyote2075@gmail.com>
2026-05-14 19:12:39 +0530

    2784-check-if-array-is-good Easy .java 1 ms (91.05%) Array 05-14-202644.6 MB (78.29%)

--- LeetCode/Easy/2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ LeetCode/Easy/2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,23 +1,34 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        Arrays.sort(nums);
-        
-        int i = 0;
-        for (int num : nums) 
-            if (num != ++i)
-                break;
-        
         int n = nums.length;
-        return (i == n && nums[n-1] == n-1);
+        boolean[] vis = new boolean[n];
+
+        for (int num : nums) {
+            if (num >= n)
+                return false;
+
+            if (vis[num])
+                if (num == n - 1)
+                    vis[0] = true;
+                else
+                    return false;
+            
+            vis[num] = true;
+        }
+
+        for (boolean val : vis) 
+            if (!val) return false;
+
+        return true;
     }
 }
-        // int n = nums.length - 1;
-        // int ans = (n * (n + 1)) / 2;
-        // ans += n;
+// int n = nums.length - 1;
+// int ans = (n * (n + 1)) / 2;
+// ans += n;
 
-        // int sum = 0;
-        // for (int num : nums)
-        //     sum += num;
+// int sum = 0;
+// for (int num : nums)
+//     sum += num;
 
-        // System.out.println(sum);
-        // return ans == sum;
+// System.out.println(sum);
+// return ans == sum;
