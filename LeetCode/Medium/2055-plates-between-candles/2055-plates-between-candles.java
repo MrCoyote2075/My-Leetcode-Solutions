bf43cd9e76b5a91528275ad55ffa6ed12c593766
Dhanush N <mrcoyote2075@gmail.com>
2026-05-18 23:04:09 +0530

    2055-plates-between-candles Medium .java 8 ms (89.37%) Array 05-18-2026150.6 MB (54.59%)

--- LeetCode/Medium/2055-plates-between-candles/2055-plates-between-candles.java
+++ LeetCode/Medium/2055-plates-between-candles/2055-plates-between-candles.java
@@ -1,70 +1,44 @@
 class Solution {
-    public int lowerBound(int x, ArrayList<Integer> arr) {
-        int l = 0, r = arr.size() - 1;
-        int ans = -1;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
-            if (x <= arr.get(mid)) {
-                ans = mid;
-                r = mid - 1;
-
-            } else
-                l = mid + 1;
-        }
-
-        return ans;
-    }
-
-    public int upperBound(int x, ArrayList<Integer> arr) {
-        int l = 0, r = arr.size() - 1;
-        int ans = -1;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
-            if (arr.get(mid) <= x) {
-                ans = mid;
-                l = mid + 1;
-
-            } else
-                r = mid - 1;
-        }
-
-        return ans;
-    }
-
     public int[] platesBetweenCandles(String s, int[][] queries) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        int n = s.length();
 
         int[] ans = new int[queries.length];
 
-        int[] pref = new int[s.length()];
-
         int cnt = 0;
-        for (int i = 0; i < s.length(); i++) {
+        int prev = -1;
+        int next = -1;
+        
+        int[] pref = new int[n];
+        int[] suff = new int[n];
+        int[] prefSum = new int[n];
+
+        for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if (ch == '|')
-                arr.add(i);
-            else
+            if (ch == '*') 
                 cnt++;
+            
+            prefSum[i] = cnt;
 
-            pref[i] = cnt;
+            if (ch == '|') 
+                prev = i;
+            
+            int ind = n - i - 1;
+            if (s.charAt(ind) == '|')
+                next = ind;
+
+            pref[i] = prev;
+            suff[ind] = next;
         }
-
-        // System.out.println(Arrays.toString(pref));
 
         for (int i = 0; i < queries.length; i++) {
 
-            int l = lowerBound(queries[i][0], arr);
-            int r = upperBound(queries[i][1], arr);
+            int l = suff[queries[i][0]];
+            int r = pref[queries[i][1]];
 
-            if (l == -1 || r == -1 || l == r || l > r)
+            if (l == -1 || r == -1 || l >= r)
                 continue;
 
-            // System.out.println(arr.get(l) + ":" + arr.get(r));
-            ans[i] = pref[arr.get(r)] - pref[arr.get(l)];
+            ans[i] = prefSum[r] - prefSum[l];
         }
 
         return ans;
