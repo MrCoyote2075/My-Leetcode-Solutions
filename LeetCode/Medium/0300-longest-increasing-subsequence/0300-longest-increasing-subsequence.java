8869d679b828a6ffd624913c17cd86ecc93f505c
Dhanush N <mrcoyote2075@gmail.com>
2026-05-25 20:57:59 +0530

    0300-longest-increasing-subsequence Medium .java 74 ms (24.01%) Array 05-25-202646 MB (68.43%)

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,32 @@
 class Solution {
     public int n;
-    public int[][] dp;
-    public int rec(int lev, int prev, int[] nums) {
+    public int[] dp;
 
-        if (lev == n)
+    public int rec(int i, int[] nums) {
+        if (i == n)
             return 0;
-
-        if (dp[lev][prev + 1] != -1)
-            return dp[lev][prev + 1];
+        
+        if (dp[i] != -1)
+            return dp[i];
 
         int ans = 0;
-
-        for (int i = lev; i < n; i++) {
-            if (prev == -1 || nums[prev] < nums[i])
-                ans = Math.max(ans, rec(i + 1 , i, nums) + 1);
-        }
-
-        return dp[lev][prev + 1] = ans;
+        for (int j = i + 1; j < n; j++)
+            if (nums[i] < nums[j])
+                ans = Math.max(ans, rec(j, nums) + 1);
+        
+        return dp[i] = ans;
     }
-
+    
     public int lengthOfLIS(int[] nums) {
         n = nums.length;
-        dp = new int[n + 2][n + 2];
 
-        for (int[] row : dp) 
-            Arrays.fill(row, -1);
+        dp = new int[n];
+        Arrays.fill(dp, -1);
 
-        return rec(0, -1, nums);
+        int ans = 0;
+        for (int i = 0 ; i < n ; i++)
+            ans = Math.max(ans, rec(i, nums) + 1);
+
+        return ans;
     }
 }
